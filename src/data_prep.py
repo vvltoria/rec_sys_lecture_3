9a0baa2cc84b81c0763d4225ff47832aea3b6650
victoria <vvtoriaa@icloud.com>
2026-05-10 13:45:35 +0300

    data preparation

--- src/data_prep.py
+++ src/data_prep.py
@@ -1,3 +1,30 @@
-def prepare_data(df, min_interactions=10, test_dayes=30):
-    pass
-    # return train_df, test_df
+import pandas as pd
+
+
+def prepare_data(df, min_interactions=10, test_days=30):
+    user_rating = df.groupby("user_id").size().reset_index(name="rating_quantity")
+    item_rating = df.groupby("item_id").size().reset_index(name="rating_quantity")
+
+    active_users = user_rating.loc[
+        user_rating["rating_quantity"] >= min_interactions, "user_id"
+    ].unique()
+    active_films = item_rating.loc[
+        item_rating["rating_quantity"] >= min_interactions, "item_id"
+    ].unique()
+
+    mask = (df["user_id"].isin(active_users)) & (df["item_id"].isin(active_films))
+    filtered_df = df[mask].copy()
+
+    max_date = filtered_df["last_watch_dt"].max()
+    cut_off_date = max_date - test_days
+
+    train_df = filtered_df[filtered_df["last_watch_dt"] < cut_off_date].copy()
+    test_df = filtered_df[filtered_df["last_watch_dt"] >= cut_off_date].copy()
+
+    train_users_ids = train_df["user_id"].unique()
+    train_item_ids = train_df["item_id"].unique()
+
+    test_df = test_df[test_df["user_id"].isin(train_users_ids)].copy()
+    test_df = test_df[test_df["item_id"].isin(train_item_ids)].copy()
+
+    return train_df, test_df
